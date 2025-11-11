d046ec0720d4804b296f4862b079b6307662d98e
sumitmanit <nimcettest@gmail.com>
2025-11-11 22:14:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (7.71%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
+        // Sumit Goswami
            int n = nums.length;
          for(int i=0; i<n; i++){
             if(nums[i]==target){
