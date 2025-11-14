b290ee18876af605bebe57db023b6dd3b86a8b80
sumitmanit <nimcettest@gmail.com>
2025-11-14 10:50:34 +0530

    Time: 5 ms (1.35%) | Memory: 45.6 MB (6.83%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,11 +1,27 @@
 class Solution {
     public boolean search(int[] nums, int target) {
 
-        // Sumimt Goswami
+        // Sumit Goswami
          Arrays.sort(nums);
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==target) return true;
+
+        int first = Arrays.binarySearch(nums, target);
+
+
+        int start = first, end = first;
+        if(first>-1){
+              while (start > 0 && nums[start - 1] == target) {
+            start--;
         }
+
+        while (end < nums.length - 1 && nums[end + 1] == target) {
+            end++;
+        }
+        if(nums[start]==target || nums[end]==target){
+            return true;
+            }
+        }
+
+        
 
         return false;
     }
