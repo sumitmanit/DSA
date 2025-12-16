56ea1aa0a639e941a4c1d385fd4cc713b3fd3308
sumitmanit <nimcettest@gmail.com>
2025-12-16 18:20:03 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (23.64%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        int rev = 0;
+        int n = digits.length;
+
+        for(int i = n-1; i>=0; i--){
+            if(digits[i]<9){
+                digits[i]++;
+                break;
+            }
+
+            digits[i] = 0;
+        }
+        
+            if(digits[0]==0){
+                int[] newArr = new int[n+1];
+                newArr[0] = 1;
+
+                return newArr;
+            }
+        
+
+
+        return digits;
+    }
+}
