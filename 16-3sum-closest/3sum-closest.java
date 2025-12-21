51e47d4f247a571cedbd44c2bfefd3a353764a6e
sumitmanit <nimcettest@gmail.com>
2025-12-21 12:48:58 +0530

    Time: 26 ms (7.66%) | Memory: 45.6 MB (33.13%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+
+        // Sumit Goswami
+
+        Arrays.sort(nums);
+        
+        int closesSum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int l = i + 1;
+            int r = nums.length - 1;
+
+            while (l < r) {
+
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if (Math.abs(sum - target) < Math.abs(closesSum - target)) {
+                    closesSum = sum;
+                } else if (sum < target) {
+                    l++;
+                } else {
+                    r--;
+                }
+
+            }
+
+        }
+
+        return closesSum;
+        
+    }
+}
