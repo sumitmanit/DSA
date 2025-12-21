7304eecf88477fbb584be190aedfc0150a5aff61
sumitmanit <nimcettest@gmail.com>
2025-12-21 17:42:48 +0530

    Time: 2 ms (99.91%) | Memory: 46.6 MB (39.48%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    // Sumit Goswami
+      static int romanNumber(char ch) {
+
+        switch (ch) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+
+    }
+    public int romanToInt(String s) {
+
+         int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int value = romanNumber(s.charAt(i));
+
+            if (i + 1 < s.length() && value < romanNumber(s.charAt(i + 1))) {
+                sum -= value;
+            } else {
+                sum += value;
+            }
+        }
+        return sum;
+    }
+
+
+    }
