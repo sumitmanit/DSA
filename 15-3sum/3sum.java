910a99ccc164cac360149a1c80ece23343b7df2c
sumitmanit <nimcettest@gmail.com>
2025-12-21 11:57:14 +0530

    Time: 32 ms (67.33%) | Memory: 60.4 MB (8.18%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -31,7 +31,9 @@ class Solution {
             r--;
 
             while (l < r && nums[l] == nums[l - 1]) l++;
-                    while (l < r && nums[r] == nums[r + 1]) r--;
+             while (l < r && nums[r] == nums[r + 1]) r--;
+
+
         } else if (sum < 0) {
             l++;
         } else {
