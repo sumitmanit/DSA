4ca2fc8b3c40246aa2353897deebaaebe2d2120b
sumitmanit <nimcettest@gmail.com>
2026-05-15 18:02:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (64.00%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,12 +1,47 @@
 class Solution {
+    // Sumit Goswami
     public int search(int[] nums, int target) {
-        // Sumit Goswami
-           int n = nums.length;
-         for(int i=0; i<n; i++){
-            if(nums[i]==target){
-                return i;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid;
             }
         }
-        return - 1;
+
+         int pivot = start;
+
+        int result = binarySearch(nums, 0, pivot - 1, target);
+
+        if (result == -1) {
+            result = binarySearch(nums, pivot, nums.length - 1, target);
+        }
+
+        return result;
     }
+
+    static int binarySearch(int[] nums, int start, int end, int target) {
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } 
+            else if (nums[mid] < target) {
+                start = mid + 1;
+            } 
+            else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
 }
