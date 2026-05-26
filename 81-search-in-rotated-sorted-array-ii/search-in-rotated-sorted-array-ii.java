1c935a8c0bb5246a76d89e8cc437151298cacca9
sumitmanit <nimcettest@gmail.com>
2026-05-26 19:07:30 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (11.71%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,28 +1,71 @@
 class Solution {
-    public boolean search(int[] nums, int target) {
+    // Sumit Goswami
+    public boolean search(int[] nums, int target) {         
 
-        // Sumit Goswami
-         Arrays.sort(nums);
+        int start = 0;
+        int end = nums.length - 1;
 
-        int first = Arrays.binarySearch(nums, target);
-
-
-        int start = first, end = first;
-        if(first>-1){
-              while (start > 0 && nums[start - 1] == target) {
-            start--;
+        int pivot = findPivotWithDuplicates(nums, start, end);
+     if (pivot == -1) {
+            return binarysearch(nums, start, end, target);
         }
-
-        while (end < nums.length - 1 && nums[end + 1] == target) {
-            end++;
-        }
-        if(nums[start]==target || nums[end]==target){
+        if(nums[pivot]==target){
             return true;
+        }
+         if(target>=nums[start]){
+          return  binarysearch(nums,start,pivot-1,target);
+        }else{
+           return binarysearch(nums,pivot+1,end,target);
+        }
+            
+            
+    }
+
+     static boolean binarysearch(int[] nums, int start, int end, int target) {
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
 
-        
-
         return false;
+    }
+     static int findPivotWithDuplicates(int[] nums, int start, int end) {
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (end > mid && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if (start < mid && nums[mid] < nums[mid - 1]) {
+                return mid - 1;
+            }
+            if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
+
+                if (start < end && nums[start] > nums[start + 1]) {
+                    return start;
+                }
+                start++;
+
+                if (end > start && nums[end] < nums[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+
+            } else if (nums[start] < nums[mid] || (nums[start] == nums[mid] && nums[mid] > nums[end])) {
+                start = mid + 1;
+
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
     }
 }
