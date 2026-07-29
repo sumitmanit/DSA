3b9f53dcb4ba9025ccb5348f098df5158e00624a
sumitmanit <nimcettest@gmail.com>
2026-07-29 16:29:57 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (16.59%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i < j) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            int startCol = 0;
+            int endCol = matrix.length - 1;
+            while (startCol <= endCol) {
+                int temp = matrix[i][startCol];
+                matrix[i][startCol] = matrix[i][endCol];
+                matrix[i][endCol] = temp;
+
+                startCol++;
+                endCol--;
+            }
+        }
+    }
+}
